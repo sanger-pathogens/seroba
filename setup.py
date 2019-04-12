4a31354f6d3dd52c62604a1fc1d48aea5d4eb70b
Anthony Underwood <email2ants@gmail.com>
2019-04-12 12:43:59 +0100

    ariba needs pymummer <=0.10.3

--- setup.py
+++ setup.py
@@ -14,7 +14,7 @@ setup(
     tests_require=['nose >= 1.3'],
     install_requires=[
         'ariba >= 2.9.1',
-        'pymummer>=0.10.2',
+        'pymummer=0.10.3',
         'PyYAML>=3.12',
         'biopython>=1.68',
         'pyfastaq>=3.15.0'
